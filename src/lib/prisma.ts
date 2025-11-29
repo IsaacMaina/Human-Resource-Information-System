a70283d7db754f312455187e95b4709cf27cca33
Isaac Maina <isaacmaina1973@gmail.com>
2025-11-30 01:45:42 +0300

    Fix Prisma client initialization for deployment environment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query', 'error', 'warn'],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL || '',
-      },
-    },
-  })
+export const prisma = global.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
